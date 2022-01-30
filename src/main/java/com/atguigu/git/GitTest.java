fdc6bd9fd317d4a172073d2732788bbdac9f5184
YTX <YTX@atguigu.com>
2022-01-30 18:16:51 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author YTX
+ * @create 2022-01-30 17:40
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
